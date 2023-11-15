20507fb7bc7f05b8aae787bc2a35329b96de6c0c
NightUmbrella <githubumbrella@gmail.com>
2023-11-15 17:10:22 +0500

    update

--- components/background.tsx
+++ components/background.tsx
@@ -1,17 +1,109 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import ReactPlayer from "react-player/lazy";
 
 const Background = ({ children }: { children: React.ReactNode }) => {
-  
+  const [videoUrl, setVideoUrl] = useState<string>("/wal1.mp4");
+  const [isValidUrl, setIsValidUrl] = useState<boolean>(true);
+  const [isModal, setIsModal] = useState<boolean>(true);
+  const [videoId, setVideoId] = useState<string>("");
+  const [isYouTube, setIsYouTube] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUrl = event.target.value;
+    setVideoUrl(inputUrl);
+  };
+
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setIsValidUrl(validateUrl(videoUrl));
+  //   setIsModal(false);
+  // };
+
+  const validateUrl = (url: string): boolean => {
+    // Perform your URL validation here
+    return url.match(/\.(mp4|ogg|webm|mov)$/) !== null;
+  };
+
+  const openModal = () => {
+    setIsModal(true);
+  };
+
+  // for youtube url ↓↓↓
+  const getYouTubeVideoId = (url: string): string => {
+    const regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    return match && match[2].length === 11 ? match[2] : "";
+  };
+
+  const validateYouTubeUrl = (id: string): boolean => {
+    // Perform your validation logic here
+    return id.length === 11; // Basic check for video ID presence
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsValidUrl(validateYouTubeUrl(videoId));
+  };
+
+ 
+
   return (
     <div className="h-screen w-screen relative overflow-hidden">
+      {/* modal button */}
+      <div className="relative z-50">
+        {isModal ? (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter video URL"
+              onChange={handleChange}
+            />
+            <button type="submit">Play</button>
+          </form>
+        ) : (
+          <h1 className="" onClick={openModal}>
+            Open modal
+          </h1>
+        )}
+      </div>
+
+      {/* modal button */}
       {/* video background */}
       <div className="absolute top-0 left-0 -z-50">
-        <video src="/wal1.mp4" autoPlay loop muted></video>
+        {/* <video src={videoUrl} autoPlay loop muted></video> */}
+
+        {/* <ReactPlayer playing={true} loop controls={false} muted url='https://www.youtube.com/watch?v=LXb3EKWsInQ' /> */}
+        {/* <video controls>
+          <source
+            src="https://www.youtube.com/watch?v=L93hyPiltLA&t=126s"
+            type="video/mp4"
+          />
+        </video> */}
+      
+      </div>
+      <div>
+          {/* <iframe
+          width="1903"
+          height="776"
+          onClick={console}
+          className="relative z-50"
+          src="https://www.youtube.com/embed/L93hyPiltLA?&autoplay=1"
+          title="How to upload youtube videos in your React App without the Iframe tag or embed URL | React Player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe> */}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ReactPlayer 
+       url='https://www.youtube.com/watch?v=_ITiwPMUzho'
+       controls={false} />
+    </main>
       </div>
       {/* video background */}
       {children}
     </div>
   );
 };
+
 
 export default Background;
