6e50de11c4d09774d22a9ea25a485790ccbd9268
NightUmbrella <githubumbrella@gmail.com>
2023-12-10 05:27:30 +0500

    refactor code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Settings from "@/components/settings/settings";
 import useModalStore from "@/zustand-store/modal.store";
-import Modal from "./modal";
-import ModalTwo from "./modal2";
 
 const Home = () => {
   const { openModal,modals } = useModalStore();
@@ -13,8 +11,6 @@ const Home = () => {
       <div className="ml-10">
         <button onClick={() => openModal("modal1")}>Open Modal 2</button>
         <button onClick={() => openModal("modal2")}>Open Modal 2</button>
-        <Modal id="modal1" />
-        <ModalTwo id="modal2"/>
       </div>
     </div>
   );
