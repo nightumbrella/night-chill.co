f6919289bd7cc58dba4bdc51c548a79d07748219
NightUmbrella <githubumbrella@gmail.com>
2023-12-10 06:13:48 +0500

    create allRoutes

--- components/modal-all.tsx
+++ components/modal-all.tsx
@@ -4,8 +4,44 @@ import { useWindowModal } from "@/zustand-store/window-modal";
 import React from "react";
 import { motion as m, AnimatePresence } from "framer-motion";
 
+
+
+const allRoutes = [
+    {
+        label:"Account Settings",
+        icon:'UserCog' ,
+    },
+    {
+        label:"Integrations",
+        icon:'Usb' ,
+    },
+    {
+        label:"Settings",
+        icon:'settings' ,
+    },
+    {
+        label:"Help",
+        icon:'badge-help' ,
+    },
+    {
+        label:"Activity",
+        icon:'activity' ,
+    },
+    {
+        label:"Notes",
+        icon:'file' ,
+    },
+    {
+        label:"Calendar",
+        icon:'calendar-check' ,
+    },
+]
+
+
 const ModalAll = () => {
   const { isModal, openModal, closeModal } = useWindowModal();
+
+
   return (
     <>
       <button className="text-white" onClick={() => openModal()}>
@@ -14,10 +50,10 @@ const ModalAll = () => {
       <AnimatePresence>
         {isModal && (
           <m.div
-            initial={{ x: '100%',opacity:0 }}
-            animate={{ x: 0,opacity:1 }}
-            exit={{ x: '-100%',opacity:0 }}
-            transition={{ duration: 0.2,ease:"easeInOut" }}
+            initial={{ x: "100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: "-100%", opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
             className="bg-[#0c0c26d7] backdrop-blur-lg border-[2px] mt-16 border-[#ababec65] rounded-lg mx-auto  max-w-5xl h-[800px]"
           >
             <div className="border-b h-[30px] border-[#ababec65] ">
