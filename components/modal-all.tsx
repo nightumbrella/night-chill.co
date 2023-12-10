3d37855844d754a5a791e368c04a783091537cf5
NightUmbrella <githubumbrella@gmail.com>
2023-12-10 05:40:55 +0500

    create windows modal store

--- components/modal-all.tsx
+++ components/modal-all.tsx
@@ -1,9 +1,11 @@
+import useModalStore from "@/zustand-store/modal.store";
 import React from "react";
 
 const ModalAll = () => {
   return (
-    <div className="bg-[#0c0c26d7] backdrop-blur-lg border-[2px] border-[#ababec65] rounded-lg mx-auto  max-w-5xl h-[800px]">
+    <div className="bg-[#0c0c26d7] backdrop-blur-lg border-[2px] mt-16 border-[#ababec65] rounded-lg mx-auto  max-w-5xl h-[800px]">
         <div className="border-b h-[30px] border-[#ababec65] "></div>
+
     </div>
 
   );
