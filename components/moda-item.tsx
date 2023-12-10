e22da4e9daaddc7e58d45868bf72e67e02ffe6fd
NightUmbrella <githubumbrella@gmail.com>
2023-12-10 07:31:04 +0500

    🦀🦀🦀🦀

--- components/moda-item.tsx
+++ components/moda-item.tsx
@@ -1,19 +1,28 @@
 "use client";
 import React from "react";
+import { LucideIcon } from "lucide-react";
 
 const ModalItem = ({
   label,
   activeLabel,
   onClick,
+  icon: Icon,
 }: {
   label: string;
+  icon: LucideIcon;
   activeLabel: string;
   onClick: (e: string) => void;
 }) => {
   const checkLabel = label === activeLabel;
-  
 
-  return <div onClick={() => onClick(label)}>modalItems</div>;
+  return (
+    <div onClick={() => onClick(label)} className="flex items-center gap-3 px-2 py-3 cursor-pointer hover:bg-[#ababec65] transition rounded-md">
+      <div>
+        <Icon className="text-slate-300"/>
+      </div>
+      {label}
+    </div>
+  );
 };
 
 export default ModalItem;
--- components/modal-all.tsx
+++ components/modal-all.tsx
@@ -4,48 +4,63 @@ import { useWindowModal } from "@/zustand-store/window-modal";
 import React, { useState } from "react";
 import { motion as m, AnimatePresence } from "framer-motion";
 import ModalItem from "./moda-item";
-import Settings from "./settings/settings";
+// import Settings from "./settings/settings";
 import Notes from "./notes";
+import Calendar from "./calendar";
+import {
+  Activity,
+  BadgeHelp,
+  CalendarCheck,
+  CheckCheck,
+  File,
+  Settings,
+  Usb,
+  UserCog,
+} from "lucide-react";
 
 const allRoutes = [
   {
     label: "Account Settings",
-    icon: "UserCog",
+    icon: UserCog,
   },
   {
     label: "Integrations",
-    icon: "Usb",
+    icon: Usb,
   },
   {
     label: "Settings",
-    icon: "settings",
+    icon: Settings,
   },
   {
     label: "Help",
-    icon: "badge-help",
+    icon: BadgeHelp,
   },
   {
     label: "Activity",
-    icon: "activity",
+    icon: Activity,
   },
   {
     label: "Notes",
-    icon: "file",
+    icon: File,
   },
   {
     label: "Calendar",
-    icon: "calendar-check",
+    icon: CalendarCheck,
   },
 ];
 
 const layout = [
-  {
-    comp: <Settings />,
-    label: "Settings",
-  },
+  //   {
+  //     comp: <Settings />,
+  //     label: "Settings",
+  //   },
   {
     comp: <Notes />,
     label: "Notes",
+  },
+  {
+    comp: <Calendar />,
+    label: "Calendar",
   },
 ];
 
@@ -59,50 +74,53 @@ const ModalAll = () => {
   };
 
   return (
-    <>
+    <div className="h-screen w-screen relative">
+        {/* shadow */}
+        <div className={`absolute w-screen h-screen -z-10 inset-x-0 ${isModal && "bg-[#1c1c2b65]"}`}></div>
       <button className="text-white" onClick={() => openModal()}>
         open modal
       </button>
       <AnimatePresence>
         {isModal && (
           <m.div
-            initial={{ x: "100%", opacity: 0, visibility: "hidden" }}
-            animate={{ x: 0, opacity: 1, visibility: "initial" }}
-            exit={{ x: "-100%", opacity: 0, visibility: "initial" }}
+            initial={{  opacity: 0, visibility: "hidden" }}
+            animate={{ opacity: 1, visibility: "initial" }}
+            exit={{  opacity: 0, visibility: "initial" }}
             transition={{ duration: 0.2, ease: "easeInOut" }}
             className={`bg-[#0c0c26d7] backdrop-blur-lg 
-            border-[2px] mt-16 border-[#ababec65] 
-            rounded-lg mx-auto  max-w-5xl h-[800px] 
-            ${activeLabelState.length > 0 ? "w-[400px] h-[600px]" : ""}
+            border-[2px] mt-16 border-[#ababec65] p-3
+            rounded-lg mx-auto  w-[400px] h-[600px]
             `}
           >
-            <div className="border-b h-[30px] border-[#ababec65] ">
+            <div className="border-b h-[35px] border-[#ababec65]">
               <button className="text-white" onClick={() => closeModal()}>
                 close
               </button>
             </div>
 
-            <div className="">
+            <div className="mt-2 text-white">
               {allRoutes.map((data) => (
                 <ModalItem
                   key={data.label}
                   label={data.label}
+                  icon={data.icon}
                   onClick={activeLabelModal}
                   activeLabel={activeLabelState}
                 />
               ))}
             </div>
 
-            <div className="grid grid-cols-2">
+            {/* <div className="absolute translate-x-2/4 translate-y-2/4  ">
               {layout.map((data) => (
-                <div key={data.label}>{data.comp}</div>
+                <div key={data.label} className="">
+                  {data.comp}
+                </div>
               ))}
-            </div>
-
+            </div> */}
           </m.div>
         )}
       </AnimatePresence>
-    </>
+    </div>
   );
 };
 
